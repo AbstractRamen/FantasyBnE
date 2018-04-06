c782fe1ae828a7d48639ed4488dd16f7bd451b7d
Jasper <jaspercheng.is@gmail.com>
2018-04-06 13:28:54 -0700

    Add wake my dyno

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all!
+
+user1 = user.create(email: 'adventurer@dream.brave', name: 'Brave adventurer', password: 123456)
+user2 = user.create(email: 'wukong@best.mountain', name: 'Sun Wukong', password: 123456)
+user3 = user.create(email: 'admin@hogwartz.edu', name: 'Albus Dumbledore ', password: 123456)
+user4 = user.create(email: 'tim@icloud.com', name: 'Time Cook', password: 123456)
+user5 = user.create(email: 'hiro@sanfransokyo.com', name: 'Hiro Hamada', password: 123456)
+user6 = user.create(email: 'xuangdi@first.emperorer', name: 'Shi Huangdi', password: 123456)
+user7 = user.create(email: 'brycehoward@famous.actress', name: 'Bryce Dallas Howard', password: 123456)
