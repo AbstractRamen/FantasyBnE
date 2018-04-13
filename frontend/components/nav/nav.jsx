576b799111c5ac9ba86b96b3f63d4f4b0a47a26a
Jasper <jaspercheng.is@gmail.com>
2018-04-12 18:49:48 -0700

    Add footer functionality

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -2,52 +2,69 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const sessionLinks = (login, openModal) => (
-  <nav className="none-user-nav">
+  <section className='all-nav'>
+    <nav className="none-user-nav">
 
-      <Link className='nav-buttons' to='/'>
-        <div className='logo'></div>
-      </Link>
+        <Link className='nav-buttons' to='/'>
+          <div className='logo'></div>
+        </Link>
 
-    <div className='none-user-nav-right'>
-      <div className='nav-buttons' >
-        <Link className='become-host' to='/listings/new'>Become a host</Link>
+      <div className='none-user-nav-right'>
+        <div className='nav-buttons' >
+          <Link className='become-host' to='/listings/new'>Become a host</Link>
+        </div>
+        <div className='nav-buttons' onClick={() => login(user)}>
+          <span className='nav-links'>Try it out!</span>
+        </div>
+        <div className='nav-buttons' onClick={() => openModal('signup')}>
+          <span className='nav-links'>Sign Up</span>
+        </div>
+        <div className='nav-buttons' onClick={() => openModal('login')}>
+          <span className='nav-links'>Log In</span>
+        </div>
       </div>
-      <div className='nav-buttons' onClick={() => login(user)}>
-        <span className='nav-links'>Try it out!</span>
+      <div className='footer'>
+        <Link to=''>Github</Link>
+        <Link to=''>Linkedin</Link>
       </div>
-      <div className='nav-buttons' onClick={() => openModal('signup')}>
-        <span className='nav-links'>Sign Up</span>
-      </div>
-      <div className='nav-buttons' onClick={() => openModal('login')}>
-        <span className='nav-links'>Log In</span>
-      </div>
+    </nav>
+    <div className='footer'>
+      <Link className='footer-links' to=''>Github</Link> &nbsp;&nbsp;&nbsp;&nbsp;
+      <Link className='footer-links' to=''>Linkedin</Link>
     </div>
-  </nav>
+  </section>
+
 );
 
 const personalGreeting = (currentUser, logout) => (
-  <nav className="none-user-nav">
+  <section className='all-nav'>
+    <nav className="none-user-nav">
 
-      <Link className='nav-buttons' to='/'>
-        <div className='logo'></div>
-      </Link>
+        <Link className='nav-buttons' to='/'>
+          <div className='logo'></div>
+        </Link>
 
-    <div className='none-user-nav-right'>
-      <div className='nav-buttons-greet' >
-        <h2 className="header-name">Hi, {currentUser.name}!</h2>
+      <div className='none-user-nav-right'>
+        <div className='nav-buttons-greet' >
+          <h2 className="header-name">Hi, {currentUser.name}!</h2>
+        </div>
+        <div className='nav-buttons'>
+          <Link className='become-host' to='/listings/new'>Become a host</Link>
+        </div>
+        <div className='nav-buttons' onClick={logout}>
+          <Link className='become-host' to='/'>Log Out</Link>
+        </div>
+        <div className='nav-buttons-avatar'>
+          <img className='avatar'
+            src={currentUser.image_url} />
+        </div>
       </div>
-      <div className='nav-buttons'>
-        <Link className='become-host' to='/listings/new'>Become a host</Link>
-      </div>
-      <div className='nav-buttons' onClick={logout}>
-        <Link className='become-host' to='/'>Log Out</Link>
-      </div>
-      <div className='nav-buttons-avatar'>
-        <img className='avatar'
-          src={currentUser.image_url} />
-      </div>
+    </nav>
+    <div className='footer'>
+      <a className='footer-links' href='https://github.com/AbstractRamen'>Github</a> &nbsp;&nbsp;&nbsp;&nbsp;
+      <a className='footer-links' href='https://www.linkedin.com/in/jtcio/'>Linkedin</a>
     </div>
-  </nav>
+  </section>
 );
 
 const Nav = ({ currentUser, logout, login, openModal }) => (
